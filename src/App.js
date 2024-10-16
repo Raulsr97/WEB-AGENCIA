48f1076052df4c00d59596a49793b286015e442c
Raulsr97 <salinasraul883@gmail.com>
2024-10-16 16:04:55 -0600

    creando paginas del sitio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import HowWeWork from './screens/HowWeWork/HowWeWork';
+import Benefits from './screens/Benefits/Benefits';
+import Contact from './screens/Contact/Contact';
+import Home from './screens/Home /Home';
+import Services from './screens/Services/Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Benefits />
+      <Contact />
+      <HowWeWork />
+      <Services />
+
     </div>
   );
 }
---
+++ src/screens/Benefits/Benefits.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Benefits = () => {
+  return (
+    <div>Benfits</div>
+  )
+}
+
+export default Benefits
